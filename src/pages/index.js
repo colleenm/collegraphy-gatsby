2c951132d50fc8055b948129a2ae9c541dd02b4c
Colleen McKenzie <colleen.mckenzie@gmail.com>
2020-11-12 17:37:25 -0800

    gee maybe I should commit some stuff! initial site setup up through having bordered main page element on about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import Header from "../components/Header"
+import Page from "../components/Page"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div>
+      <Header currentPage={'home'} />
+      <div className="page-container">
+        <div className="double-border">design page</div>
+      </div>
+    </div>
+  )
 }
